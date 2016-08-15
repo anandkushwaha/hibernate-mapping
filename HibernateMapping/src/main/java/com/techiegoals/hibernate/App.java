d64893822356cd0e31f7f060d4da11657faa80c3
anandkushwaha <anandkushwahakiet@gmail.com>
2016-08-15 17:16:52 +0530

    Added Hibernate Mapping

--- HibernateMapping/src/main/java/com/techiegoals/hibernate/App.java
+++ HibernateMapping/src/main/java/com/techiegoals/hibernate/App.java
@@ -1,5 +1,11 @@
 package com.techiegoals.hibernate;
 
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.techiegoals.util.HibernateUtil;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println("Maven + Hibernate + Oracle");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		DBUser user = new DBUser();
+
+		user.setUserId(100);
+		user.setUsername("superman");
+		user.setCreatedBy("system");
+		user.setCreatedDate(new Date());
+
+		session.save(user);
+		session.getTransaction().commit();
     }
 }
